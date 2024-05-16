4cc63108575c393596c9c9e15c85718a40e82ffd
Janne <jannehyttinen1@gmail.com>
2024-05-16 19:23:48 +0300

    for prod

--- chess-api/app.py
+++ chess-api/app.py
@@ -71,3 +71,6 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(host="0.0.0.0", port=8000, debug=True)
+else:
+    with app.app_context():
+        db.create_all()
